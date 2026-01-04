581651ea904b2ffcb7cd12d2a673128fbddcef24
Tomer Nosrati <tomernos1@gmail.com>
2026-01-04 20:38:44 +0200

    Change VPC module to version range ~> 6.0 as 6.5.1 may not exist in registry

--- infra-tf/main.tf
+++ infra-tf/main.tf
@@ -5,7 +5,7 @@ data "aws_ecrpublic_authorization_token" "token" {
 module "vpc" {
   for_each = var.vpcs
   source   = "terraform-aws-modules/vpc/aws"
-  version  = "6.5.1"  # Pinned to exact version for reproducibility (was: ~> 6.0)
+  version  = "~> 6.0"  # Using version range - will use latest 6.x.x (6.5.1 may not exist in registry)
 
   # Details
   name            = "${local.region_prefix}-vpc"
